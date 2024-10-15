d9370c7f76a996c80a815dad5721e9e421705a69
678156 <678156@stud.hvl.no>
2024-10-15 14:12:06 +0200

    Update GPSComputer.java

--- src/no/hvl/dat100ptc/oppgave4/GPSComputer.java
+++ src/no/hvl/dat100ptc/oppgave4/GPSComputer.java
@@ -32,100 +32,67 @@ public class GPSComputer {
 	public double totalDistance() {
 
 		double totalDistance = 0;
-		
+
 		for (int i = 0; i < gpspoints.length - 1; i++) {
 			double distance = GPSUtils.distance(gpspoints[i], gpspoints[i + 1]);
 			totalDistance += distance;
 		}
 
 		return totalDistance;
-		
+
 	}
 
 	public double totalElevation() {
 
 		double totalElevation = 0;
-		
-		
-		for (int i = 0; i < gpspoints.length -1; i++) {
-			double elevationDifference = gpspoints[i +1].getElevation() - gpspoints[i].getElevation();
+
+		for (int i = 0; i < gpspoints.length - 1; i++) {
+			double elevationDifference = gpspoints[i + 1].getElevation() - gpspoints[i].getElevation();
 			if (elevationDifference > 0) {
 				totalElevation += elevationDifference;
 			}
 		}
-			return totalElevation;
+		return totalElevation;
 
 	}
 
 	public int totalTime() {
-		
+
 		int totalTime = 0;
-		
+
 		for (int i = 0; i < gpspoints.length - 1; i++) {
 			int time1 = gpspoints[i].getTime();
-			int time2 = gpspoints[i+1].getTime();
-			totalTime += time2-time1;
-		}
-
-<<<<<<< Updated upstream
-		return totalTime;
-		
-=======
-		int totalTime = 0;
-		
-		for (int i = 0; i < gpspoints.length -1; i++) {
-			int timeDifference = gpspoints[i +1].getTime() - gpspoints[i].getTime();
-			if (timeDifference > 0) {
-				totalTime += timeDifference;
-			}
+			int time2 = gpspoints[i + 1].getTime();
+			totalTime += time2 - time1;
 		}
 		return totalTime;
-		
-
->>>>>>> Stashed changes
 	}
-	
-			//	d)
-			//	Implementer metoden
-			//
-			//	public double[] speeds()
-			//	som skal returnere en tabell med gjennomsnitshastigheter målt i m/s mellom hver av de punktene 
-			//	vi har beveget oss mellom. 
-			//	Dvs. første inngang i tabellen skal være hastigheten vi beveget oss med mellom punkt 0 og punkt 1, 
-			//	andre inngang hastigheten mellom punkt 1 og 2 osv. 
-			//	Hvis antall GPS datapunker er N da vil lengden av den tabellen som returneres være N-1.
 
 	public double[] speeds() {
 
 		double[] speeds = new double[gpspoints.length - 1];
-		
-	    for (int i = 0; i < gpspoints.length - 1; i++) {
 
-<<<<<<< Updated upstream
-	        speeds[i] = GPSUtils.speed(gpspoints[i], gpspoints[i + 1]);
-	    }
-=======
-		
->>>>>>> Stashed changes
+		for (int i = 0; i < gpspoints.length - 1; i++) {
 
-	    return speeds;
+			speeds[i] = GPSUtils.speed(gpspoints[i], gpspoints[i + 1]);
+		}
+
+		return speeds;
 	}
 
 	public double maxSpeed() {
 
 		double maxSpeed = 0;
 
-    for (int i = 0; i < gpspoints.length - 1; i++) {
-        double speed = GPSUtils.speed(gpspoints[i], gpspoints[i + 1]);
-        if (speed > maxSpeed) {
-            maxSpeed = speed;
-        }
-    }
+		for (int i = 0; i < gpspoints.length - 1; i++) {
+			double speed = GPSUtils.speed(gpspoints[i], gpspoints[i + 1]);
+			if (speed > maxSpeed) {
+				maxSpeed = speed;
+			}
+		}
 
-    return maxSpeed;
-}
-
-
+		return maxSpeed;
+	}
 
 	public double averageSpeed() {
 
